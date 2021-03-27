50e9e2761390ee5b90c89b4d6e94d93159b00fc9
Stanislav Kosáček <stanislav.kosacek@qop.cz>
2021-03-27 14:06:59 +0100

    add hours to countdown

--- www/src/front/js/countdown.js
+++ www/src/front/js/countdown.js
@@ -3,16 +3,17 @@ export const printCountdownTime = (endTime, element) => {
 	let now = new Date().getTime();
 	const distance = end - now;
 
-	//var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 	if (seconds < 0) {
 		minutes = 0;
 		seconds = 0;
+		hours = 0;
 	}
 
-	element.textContent = `${ String(minutes).padStart(2, '0')} : ${ String(seconds).padStart(2, '0')}`;
+	element.textContent = `${ hours > 0 ? String(hours).padStart(2, '0') + " : " : ""}${ String(minutes).padStart(2, '0')} : ${ String(seconds).padStart(2, '0')}`;
 }
 
 export const printCountdownText = (text, element) => {
