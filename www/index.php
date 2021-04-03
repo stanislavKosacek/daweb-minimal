411faf4c3c82ff87c3772219042d19bded3bf165
Stanislav Kosáček <stanislav.kosacek@qop.cz>
2021-04-03 17:24:14 +0200

    update index.php for endora hosting

--- www/index.php
+++ www/index.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
-
+if ('https' === getenv('HTTP_X_FORWARDED_PROTO')) {
+	\Nette\Http\Url::$defaultPorts['https'] = (int) getenv('SERVER_PORT');
+}
 App\Bootstrap::boot()
 	->createContainer()
 	->getByType(Nette\Application\Application::class)
